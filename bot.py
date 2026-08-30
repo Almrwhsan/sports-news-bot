a9f457cd51b77f89eeb0f42509a50c371d316ea7
Almrwhsan <158107254+Almrwhsan@users.noreply.github.com>
2026-08-31 02:40:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,23 @@
 from datetime import datetime, timezone
+from database import init_database
 
-print("===================================")
-print("   SPORTS NEWS BOT")
-print("===================================")
-print("Bot is working successfully!")
-print("UTC time:", datetime.now(timezone.utc).isoformat())
-print("===================================")
+
+def main():
+    print("===================================")
+    print("   SPORTS NEWS BOT")
+    print("===================================")
+
+    init_database()
+
+    print("Database initialized successfully!")
+    print("Bot is working successfully!")
+    print(
+        "UTC time:",
+        datetime.now(timezone.utc).isoformat()
+    )
+
+    print("===================================")
+
+
+if __name__ == "__main__":
+    main()
